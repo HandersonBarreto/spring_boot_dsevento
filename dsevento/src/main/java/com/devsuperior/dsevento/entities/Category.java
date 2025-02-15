239ab6c72baf39b568852beebcd19b86e010102b
Handerson Barreto <handersonaires@gmail.com>
2025-02-15 14:56:06 -0300

    Configurado banco H2, Category e Activity

---
+++ dsevento/src/main/java/com/devsuperior/dsevento/entities/Category.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsevento.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+
+    @OneToMany(mappedBy = "category")
+    private List<Activity> activities = new ArrayList<>();
+
+    public Category(){
+
+    }
+
+    public Category(String description, Integer id) {
+        this.description = description;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
